caed87c78b781bb5baf436037f6c90f319a96b85
twin <turingcompleted@gmail.com>
2023-07-28 14:25:20 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-repo_name = "TheBloke/FreeWilly2-GPTQ"
-model_name = "FreeWilly2-GPTQ"
+repo_name = "TheBloke/airoboros-l2-13B-gpt4-1.4.1-GPTQ"
+model_name = "airoboros-l2-13B-gpt4-1.4.1-GPTQ"
 model_basename = "gptq_model-4bit-32g.safetensors"
