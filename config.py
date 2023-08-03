183b52274838e0ae4ccbed42f719a49a525cf845
twin <turingcompleted@gmail.com>
2023-08-02 18:53:37 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-repo_name = "TheBloke/airoboros-l2-13B-gpt4-1.4.1-GPTQ"
-model_name = "airoboros-l2-13B-gpt4-1.4.1-GPTQ"
-model_basename = "gptq_model-4bit-32g.safetensors"
+repo_name = "TheBloke/airoboros-l2-70B-GPT4-2.0-GPTQ"
+model_name = "airoboros-l2-70B-GPT4-2.0-GPTQ"
+model_basename = "gptq_model-4bit--1g.safetensors"
