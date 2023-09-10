79146cb9a45ddde7be30381b697d3a53dc9b0186
twin <turingcompleted@gmail.com>
2023-09-10 01:34:29 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-repo_name = "TheBloke/Airoboros-L2-70B-2.1-GPTQ"
-model_name = "Airoboros-L2-70B-2.1-GPTQ"
+repo_name = "TheBloke/ORCA_LLaMA_70B_QLoRA-GPTQ"
+model_name = "ORCA_LLaMA_70B_QLoRA-GPTQ"
 model_basename = "gptq_model-4bit-32g.safetensors"
