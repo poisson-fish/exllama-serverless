b18378c4b8c7fc69f5892a8d851a76b2caa136f1
twin <twin@twin.com>
2023-10-07 01:41:20 -0700

    hurr durr

--- inference.py
+++ inference.py
@@ -15,19 +15,19 @@ class Predictor:
         self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95)    
 
     def predict(self, settings):
-        self.sampling_params = SamplingParams(temperature=settings.temperature, 
-                                              top_p=settings.top_p,
-                                              top_k=settings.top_k,
-                                              early_stopping=settings.early_stopping,
-                                              n=settings.n,
-                                              best_of=settings.best_of,
-                                              presence_penalty=settings.presence_penalty,
-                                              frequency_penalty=settings.frequency_penalty,
-                                              use_beam_search=settings.use_beam_search,
-                                              length_penalty=settings.length_penalty,
-                                              stop=settings.stop,
-                                              ignore_eos=settings.ignore_eos,
-                                              max_tokens=settings.max_tokens
+        self.sampling_params = SamplingParams(temperature=settings["temperature"], 
+                                              top_p=settings["top_p"],
+                                              top_k=settings["top_k"],
+                                              early_stopping=settings["early_stopping"],
+                                              n=settings["n"],
+                                              best_of=settings["best_of"],
+                                              presence_penalty=settings["presence_penalty"],
+                                              frequency_penalty=settings["frequency_penalty"],
+                                              use_beam_search=settings["use_beam_search"],
+                                              length_penalty=settings["length_penalty"],
+                                              stop=settings["stop"],
+                                              ignore_eos=settings["ignore_eos"],
+                                              max_tokens=settings["max_tokens"]
                                               ) 
         return self.generate_to_eos(settings.prompt)
     
