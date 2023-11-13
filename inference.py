89ecbd21b07a391fe16d2e51783da49275632478
twin <turingcompleted@gmail.com>
2023-11-12 19:59:39 -0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ class Predictor:
         model_directory = f"/runpod-volume/{model_name}"  
         # model_directory = f"./models/{model_name}"
         print("Loading model...")
-        self.llm = LLM(model=model_directory, gpu_memory_utilization=0.9, quantization='awq')
+        self.llm = LLM(model=model_directory, gpu_memory_utilization=0.45, dtype='fp16', quantization='awq')
         self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95)    
 
     def predict(self, settings):
